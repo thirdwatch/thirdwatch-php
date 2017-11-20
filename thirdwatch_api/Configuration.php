aee1010f851c1ab3ded5602823eb540d49276eee
tw-tarun <tarun@thirdwatch.ai>
2017-11-20 13:35:23 +0530

    Live SDK Updated

--- thirdwatch_api/Configuration.php
+++ thirdwatch_api/Configuration.php
@@ -88,7 +88,7 @@ class Configuration
      *
      * @var string
      */
-    protected $host = 'https://localhost/event';
+    protected $host = 'https://api.thirdwatch.ai/event';
 
     /**
      * Timeout (second) of the HTTP request, by default set to 0, no timeout
